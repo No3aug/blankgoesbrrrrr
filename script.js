fcf6ae7e246bd8ba410f32e836584bf266201750
No3aug <120092740+No3aug@users.noreply.github.com>
2022-12-08 10:37:03 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+<script>
+function changeText(id) {
+  id.innerHTML = "Welcome in The Game's Vault"
+}  
+
+function mOut(obj) {
+  obj.innerHTML = "Mouse Over Me"
+}
+  
+function mOver(obj) {
+  obj.innerHTML = "Thank You"
+}
+</script>
